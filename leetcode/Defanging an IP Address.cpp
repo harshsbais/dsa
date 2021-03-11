6737b9ce1b8fd15675a08b58b10677169e5f0522
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-11 23:41:16 +0530

    more random easy leetcode probs

---
+++ leetcode/Defanging an IP Address.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    string defangIPaddr(string address)
+    {
+        string ans = "";
+        for (int i = 0; i < address.length(); i++)
+        {
+            if (address[i] == '.')
+            {
+                ans += "[";
+                ans += address[i];
+                ans += "]";
+            }
+            else
+            {
+                ans += address[i];
+            }
+        }
+        return ans;
+    }
+};
